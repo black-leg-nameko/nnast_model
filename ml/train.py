98e109504ffe6b1ac7caf3795b3d8837b6a526fd
black-leg-nameko <sabuchan1112@icloud.com>
2025-12-16 15:13:37 +0900

    fix train.py

--- ml/train.py
+++ ml/train.py
@@ -419,7 +419,7 @@ def main():
     
     # Learning rate scheduler
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode='min', factor=0.5, patience=3, verbose=True
+        optimizer, mode='min', factor=0.5, patience=3
     )
     
     # Training loop
